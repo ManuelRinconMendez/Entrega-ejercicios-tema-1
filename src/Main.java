4fd1e645a508cd06a8182d7a024de441047aa6b9
manuelrinconmendez <manuel.rinconmendez@outlook.com>
2022-07-23 22:03:10 -0500

    subiendo tarea

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // int, short, long, double, float, boolean, byte, char
+        byte primero = 1;
+        int segundo = 1;
+        long tercero = 123;
+        short cuarto = 12345;
+        double quinto = 12121.12;
+        float sexto = 12;
+        boolean septimo = true;
+        byte octavo = 1;
+        char noveno = 'z';
+
+        System.out.println("Este dato es byte: " + primero);
+        System.out.println("Este dato es int: " + segundo);
+        System.out.println("Este dato es long: " + tercero);
+        System.out.println("Este dato es short: " + cuarto);
+        System.out.println("Este dato es double: " + quinto);
+        System.out.println("Este dato es float: " + sexto);
+        System.out.println("Este dato es boolean: " + septimo);
+        System.out.println("Este dato es octavo: " + octavo);
+        System.out.println("Este dato es noveno: " + noveno);
+
+    }
+
+}
